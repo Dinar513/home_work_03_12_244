774d3a4f91d23f310e9a6783ba2d8cd955db4234
dinar valeev <dinarvaleev513@gmail.com>
2024-12-22 12:09:07 +0500

    написал функцию filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+
+def filter_by_state(transactions, state='EXECUTED'):
+    """ Фильтрует список словарей по значению ключа `state` """
+    return [transaction for transaction in transactions if transaction.get('state') == state]
+
+# Пример использования:
+transactions = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+]
+
+# Выход функции со статусом по умолчанию 'EXECUTED':
+executed_transactions = filter_by_state(transactions)
+print(executed_transactions)
+
+# Выход функции, если вторым аргументом передано 'CANCELED':
+canceled_transactions = filter_by_state(transactions, 'CANCELED')
+print(canceled_transactions)
+
