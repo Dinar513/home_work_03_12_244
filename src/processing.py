8e229b4a3326945b8aa87c245a89db4b26318809
dinar valeev <dinarvaleev513@gmail.com>
2024-12-22 12:18:34 +0500

    написал функцию sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -21,3 +21,22 @@ print(executed_transactions)
 canceled_transactions = filter_by_state(transactions, 'CANCELED')
 print(canceled_transactions)
 
+from datetime import datetime
+
+
+def sort_by_date(data, descending=True):
+    """Сортирует список словарей по дате (по ключу 'date')"""
+    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=descending)
+
+
+# Пример входных данных
+transactions = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+# Проверка работы функции
+sorted_transactions = sort_by_date(transactions)
+print(sorted_transactions)
