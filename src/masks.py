023e5c88edec1c391fe9301224ab859cdf8540a8
dinar valeev <dinarvaleev513@gmail.com>
2024-12-23 17:04:36 +0500

    fixes masks.py and write test_masks

--- src/masks.py
+++ src/masks.py
@@ -9,9 +9,11 @@ def get_mask_account(account_num: Union[str]) -> str:
 print(get_mask_account("73654108430135874305"))
 
 
+
 def get_mask_card_number(card_num: Union[str]) -> str:
     """Функция для получения маски номера карты"""
+    if not isinstance(card_num, str) or len(card_num) < 6:
+        return "Некорректный номер карты"
     return f"{card_num[:4]} {card_num[4:6]}** **** {card_num[-4:]}"
-
 
 print(get_mask_card_number("7000792289606361"))
