868c94bcc0b9dec8a5a7d5bbdae6e3cac0c08391
dinar valeev <dinarvaleev513@gmail.com>
2024-12-23 23:54:42 +0500

    mypy finish

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,7 +1,7 @@
 from src.processing import filter_by_state, sort_by_date, transactions
 
 
-def test_filter_by_state():
+def test_filter_by_state() -> None:  # Аннотация возвращаемого типа
     # Тест на фильтрацию по статусу 'EXECUTED'
     executed_transactions = filter_by_state(transactions)
     assert executed_transactions == [
@@ -25,7 +25,7 @@ def test_filter_by_state():
     assert empty_transactions == [], "Ошибка: ожидались пустые результаты для статуса 'UNKNOWN'"
 
 
-def test_sort_by_date():
+def test_sort_by_date() -> None:  # Аннотация возвращаемого типа
     # Тест на сортировку по убыванию
     sorted_transactions_desc = sort_by_date(transactions)
     assert sorted_transactions_desc == [
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,29 +1,26 @@
 import unittest
-
 from src.widget import get_date, mask_account_card
 
+class Test_Mask_Account_Card(unittest.TestCase):
+    def test_mask_maestro(self) -> None:
+        self.assertEqual(mask_account_card("Maestro 7000792289606361"), "Maestro 7000 79  6361")
 
-class test_mask_account_card(unittest.TestCase):
-    def test_mask_maestro(self):
-        self.assertEqual(mask_account_card("Maestro 7000792289606361"), "Maestro 7000 79** **** 6361")
+    def test_masks_chet(self) -> None:
+        self.assertEqual(mask_account_card("Счет 73654108430135874305"), "Счет 7365 41  4305")
 
-    def test_mask_schet(self):
-        self.assertEqual(mask_account_card("Счет 73654108430135874305"), "Счет 7365 41** **** 4305")
+    def test_mask_master_card(self) -> None:
+        self.assertEqual(mask_account_card("MasterCard 7158300734726758"), "MasterCard 7158 30  6758")
 
-    def test_mask_master_card(self):
-        self.assertEqual(mask_account_card("MasterCard 7158300734726758"), "MasterCard 7158 30** **** 6758")
+    def test_mask_visa_classic(self) -> None:
+        self.assertEqual(mask_account_card("Visa Classic 73654108430135874305"), "Visa Classic 7365 41  4305")
 
-    def test_mask_visa_classic(self):
-        self.assertEqual(mask_account_card("Visa Classic 73654108430135874305"), "Visa Classic 7365 41** **** 4305")
+    def test_mask_visa_gold(self) -> None:
+        self.assertEqual(mask_account_card("Visa Gold 7000792289606361"), "Visa Gold 7000 79  6361")
 
-    def test_mask_visa_gold(self):
-        self.assertEqual(mask_account_card("Visa Gold 7000792289606361"), "Visa Gold 7000 79** **** 6361")
-
-    def test_unknown_type(self):
+    def test_unknown_type(self) -> None:
         with self.assertRaises(ValueError) as context:
             mask_account_card("UnknownType 1234567890123456")
         self.assertEqual(str(context.exception), "Неизвестный тип карты или счета")
-
 
 test_dates = [
     "2024-03-11T02:26:18.671407",  # Обычный случай
@@ -37,4 +34,3 @@ test_dates = [
 
 for date in test_dates:
     print(f"Входная дата: '{date}' -> Результат: '{get_date(date)}'")
-
